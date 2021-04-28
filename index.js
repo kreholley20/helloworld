cba79725b99dd049c90dcb7c987173c5d1b6cd03
Kre Holley <62866789+kreholley20@users.noreply.github.com>
2021-04-28 11:34:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.use(express.static(path.join(__dirname, "public"),{
 }));
 
 app.get("/", (req, res) =>{
-    res.render("/viewpost");
+    res.redirect("/viewpost");
 });
 
 //create a new account

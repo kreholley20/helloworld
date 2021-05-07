68212bd83d8caa1fe729ed618fc5034a568167fb
Kre Holley <62866789+kreholley20@users.noreply.github.com>
2021-05-06 21:45:03 -0500

    Add files via upload

--- public/js/tdee.js
+++ public/js/tdee.js
@@ -6,17 +6,21 @@ const errorContainer = document.querySelector("#errorContainer");
 async function logTDEE (event) {
     event.preventDefault();
     const weight = document.querySelector("#weight").value;
-    
+    const age = document.querySelector("#age").value;
+    const gender = document.querySelector("#gender").value;
+    const activity = document.querySelector("#activity").value;
+    const height = document.querySelector("#height").value;
+    const goal = document.querySelector("#goal").value;
     try {
         const response = await fetch(`${window.location.origin}/counter`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({weight})
+            body: JSON.stringify({weight, age, gender, activity, height, goal})
         });
 
-        console.log(response);
+        console.log(response + "the response is: ");
 
         if (response.ok){
             tdeeForm.submit();
@@ -31,16 +35,16 @@ async function logTDEE (event) {
                 console.error(err);
                 errorContainer.classList.remove("hidden");
                 const errorMessage = errorContainer.querySelector("#errorMessage");
-                errorMessage.textContent = "Could not calculate";
+                errorMessage.textContent = "Could not calculate catch err";
             }
         } else if (response.status === 500) {
             errorContainer.classList.remove("hidden");
             const errorMessage = errorContainer.querySelector("#errorMessage");
-            errorMessage.textContent = "Could not calculate";
+            errorMessage.textContent = "Could not calculate 500";
         } else {
             errorContainer.classList.remove("hidden");
             const errorMessage = errorContainer.querySelector("#errorMessage");
-            errorMessage.textContent = "Could not calculate";
+            errorMessage.textContent = "Could not calculate else";
         }
         
     } catch (err) {
@@ -55,25 +59,7 @@ async function logTDEE (event) {
 
 
 function show() {
-    errorContainer.classList.add("hidden");
+    document.querySelector("#errorContainer").classList.add("hidden");
 }
-
 
 tdeeForm.addEventListener('submit', logTDEE);
-
-function toggle(value) {
-    const male = document.querySelector(".male");
-    const female = document.querySelector(".female");
-
-    //if female, hide male select
-    if (value === "1"){
-        console.log("female" + value);
-        female.style.display = 'block'
-        male.style.display = 'none';
-    } else {
-        console.log("male" + value);
-        male.style.display = 'block';
-        female.style.display = 'none';
-    }
-}
-
